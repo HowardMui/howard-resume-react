4f5352e44bf6146d1cb0b231152e396fab472914
HowardMui <howardmui.2020@gmail.com>
2023-05-01 23:24:44 +0800

    refactor: add animation in experienceContainer

--- src/app/pages/ExperienceContainer/ExperienceContainer.tsx
+++ src/app/pages/ExperienceContainer/ExperienceContainer.tsx
@@ -6,6 +6,9 @@ import {
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
 import { MdOutlineAdd } from 'react-icons/md';
+import { motion } from 'framer-motion';
+import { fadeInFromTop } from 'utils/motion';
+import { SectionWrapper } from 'HOC';
 
 const ExperienceContainer = () => {
   const [timelineIndex, setTimeLineIndex] = useState<number>(3);
@@ -21,34 +24,43 @@ const ExperienceContainer = () => {
   };
 
   return (
-    <div>
-      <div>Experience</div>
-      <VerticalTimeline lineColor={'#ffc107'} layout="2-columns">
-        {MT_JOURNEY.slice(0, timelineIndex).map((exp, index) => (
-          <TimeLine data={exp} key={index} />
-        ))}
-        {MT_JOURNEY.length !== timelineIndex && (
-          <VerticalTimelineElement
-            icon={
-              <div
-                className="text-white w-100 h-100"
-                style={{ cursor: 'pointer' }}
-              >
-                <MdOutlineAdd />
-              </div>
-            }
-            iconOnClick={curTimelineIndex}
-            iconStyle={{
-              background: '#3f51b5',
-              // boxShadow: '#ffc107 0px 1px 4px, #ffc107 0px 0px 0px 3px',
-              boxShadow:
-                '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)',
-            }}
-          />
-        )}
-      </VerticalTimeline>
-    </div>
+    <>
+      <div style={{ marginBottom: '100px' }}>
+        <motion.div variants={fadeInFromTop()}>
+          <h2 className={` text-center`}>Work experience & Education</h2>
+          <p className={` text-center`}>
+            My previous jobs and my qualifications.
+          </p>
+        </motion.div>
+      </div>
+
+      <div className="mt-5">
+        <VerticalTimeline lineColor={'#ffc107'} layout="2-columns">
+          {MT_JOURNEY.slice(0, timelineIndex).map((exp, index) => (
+            <TimeLine data={exp} key={index} />
+          ))}
+          {MT_JOURNEY.length !== timelineIndex && (
+            <VerticalTimelineElement
+              icon={
+                <div
+                  className="text-white w-100 h-100"
+                  style={{ cursor: 'pointer' }}
+                >
+                  <MdOutlineAdd />
+                </div>
+              }
+              iconOnClick={curTimelineIndex}
+              iconStyle={{
+                background: '#3f51b5',
+                boxShadow:
+                  '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)',
+              }}
+            />
+          )}
+        </VerticalTimeline>
+      </div>
+    </>
   );
 };
 
-export default ExperienceContainer;
+export default SectionWrapper(ExperienceContainer, '');
