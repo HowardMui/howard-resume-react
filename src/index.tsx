c19d0d0e5d0650fdce22ceddbea1d6649124bac6
HowardMui <howardmui.2020@gmail.com>
2023-05-01 17:27:44 +0800

    feat: add library css in root index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app/pages/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-vertical-timeline-component/style.min.css';
 
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 ReactDOM.render(
