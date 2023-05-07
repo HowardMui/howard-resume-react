41f750712d17b9cf5b74d55d137f869f20e250e6
HowardMui <howardmui.2020@gmail.com>
2023-05-08 02:40:05 +0800

    feat: add react-app-end.d.ts (file for svg)

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,10 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'lottie-player': any;
+  }
+}
