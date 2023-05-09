63fc4a15c4a569ce0cf8f7c41c801a4959b1a469
HowardMui <howardmui.2020@gmail.com>
2023-05-10 01:14:16 +0800

    feat: add ImageExplore.tsx, Modal

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import App from './app/pages/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-vertical-timeline-component/style.min.css';
 import ReactDOM from 'react-dom/client';
+import Modal from 'react-modal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+Modal.setAppElement('#root');
 root.render(
   <React.StrictMode>
     <App />
