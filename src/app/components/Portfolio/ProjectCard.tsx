8771a8348740b6b3af1b428c4a612b0d1917fa91
HowardMui <howardmui.2020@gmail.com>
2023-05-10 01:15:25 +0800

    style: project card

--- src/app/components/Portfolio/ProjectCard.tsx
+++ src/app/components/Portfolio/ProjectCard.tsx
@@ -1,11 +1,11 @@
-import { AnimatePresence, MotionConfig, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { PortfolioData } from 'models';
-import React from 'react';
-import { Badge } from 'react-bootstrap';
+import React, { useState } from 'react';
 import { fadeIn } from 'utils/motion';
 import GithubIcon from 'assets/icons/github.png';
 import { BiLink } from 'react-icons/bi';
 import { FiZoomIn } from 'react-icons/fi';
+import { ImageExplore } from '../Common';
 
 interface Props {
   data: PortfolioData;
@@ -15,6 +15,7 @@ interface Props {
 const ProjectCard = ({ data, index }: Props) => {
   const { video, image, url, gitHubLink, title, content, tags, category } =
     data;
+  const [open, setOpen] = useState<boolean>(false);
 
   const testTransform = () => {
     return {
@@ -41,9 +42,13 @@ const ProjectCard = ({ data, index }: Props) => {
     };
   };
 
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <motion.div className="p-4" key={`${category}-${title}`}>
+      <motion.div className="p-4 card-container" key={`${category}-${title}`}>
         <div className="media-div">
           {video ? (
             <>
@@ -56,33 +61,6 @@ const ProjectCard = ({ data, index }: Props) => {
               >
                 <source src={video} />
               </video>
-              <div className="link-wrapper">
-                <div className="github-link link-div">
-                  <a
-                    href={gitHubLink ? gitHubLink : ''}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img
-                      src={GithubIcon}
-                      alt="github"
-                      className=""
-                      width={25}
-                      height={25}
-                    />
-                  </a>
-                </div>
-                <div className="website-link link-div">
-                  <a
-                    href={url ? url : ''}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-white"
-                  >
-                    <BiLink />
-                  </a>
-                </div>
-              </div>
             </>
           ) : (
             <>
@@ -93,27 +71,57 @@ const ProjectCard = ({ data, index }: Props) => {
                 height={'100%'}
               />
               <div className="link-wrapper">
-                <div className=" link-div">
+                <div className=" link-div" onClick={() => setOpen(true)}>
                   <FiZoomIn />
                 </div>
               </div>
             </>
           )}
+          <div className="link-wrapper">
+            {gitHubLink && (
+              <div className="github-link link-div">
+                <a href={gitHubLink} target="_blank" rel="noreferrer">
+                  <img
+                    src={GithubIcon}
+                    alt="github"
+                    className=""
+                    width={25}
+                    height={25}
+                  />
+                </a>
+              </div>
+            )}
+            {url && (
+              <div className="website-link link-div">
+                <a
+                  href={url ? url : ''}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-white"
+                >
+                  <BiLink />
+                </a>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="content-div mt-4">
+        <div className="content-div mt-3">
           <h3 className="">{title}</h3>
-          <div className="mt-3">{content}</div>
-          <div className="d-flex mt-4">
-            {tags.length > 0
-              ? tags.map((tag, index) => (
-                  <Badge key={index}>
-                    <div className="">#{tag}</div>
-                  </Badge>
-                ))
-              : null}
+          <div className="content-section-div">
+            <div className="mt-3">{content}</div>
+            <div className="d-flex mt-4">
+              {tags.length > 0
+                ? tags.map((tag, index) => (
+                    <div key={index} className="me-1 badge badge-div">
+                      <div className="text-dark">#{tag}</div>
+                    </div>
+                  ))
+                : null}
+            </div>
           </div>
         </div>
       </motion.div>
+      <ImageExplore visible={open} onClose={onClose} />
     </>
   );
 };
--- src/constant/Portfolio.ts
+++ src/constant/Portfolio.ts
@@ -3,18 +3,22 @@ import { PortfolioData } from 'models';
 export const PORTFOLIO_DATA: PortfolioData[] = [
   {
     category: 'my-work',
-    image: '',
+    image: null,
+    // image: require('assets/images/Portfolio/MyWeb/CompanyWeb/home-page.png'),
+    // video: null,
     video: require('assets/video/React-company-website.mp4'),
     url: 'https://howardmui-company.netlify.app/',
     gitHubLink: 'https://github.com/HowardMui/HowardMui-Company-ReactProject',
-    title: 'My React Company Website',
+    title: 'My Company Website',
     content:
       'Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.',
     tags: ['React', 'Nodejs', 'MongoDb'],
   },
   {
     category: 'my-work',
-    image: '',
+    image: null,
+    // image: require('assets/images/Portfolio/MyWeb/FetchWeb/fetch-page.png'),
+    // video: null,
     video: require('assets/video/HowardMui-fetch-project.mp4'),
     url: 'https://howardmui-fetchpexels-project.netlify.app/',
     gitHubLink: 'https://github.com/HowardMui/Howard-fetchAPI-pexels',
@@ -41,7 +45,8 @@ export const PORTFOLIO_DATA: PortfolioData[] = [
     url: null,
     gitHubLink: null,
     title: 'SV CMS',
-    content: '',
+    content:
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
     tags: ['Typescript', 'Reactjs', 'Nodejs'],
   },
   {
@@ -51,7 +56,8 @@ export const PORTFOLIO_DATA: PortfolioData[] = [
     url: null,
     gitHubLink: null,
     title: 'Weave-living CMS',
-    content: '',
+    content:
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
     tags: ['Typescript', 'React', 'Nodejs'],
   },
   {
@@ -61,7 +67,8 @@ export const PORTFOLIO_DATA: PortfolioData[] = [
     url: null,
     gitHubLink: null,
     title: 'Fader CMS',
-    content: '',
+    content:
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
     tags: ['Typescript', 'React', 'Nodejs'],
   },
   {
@@ -71,7 +78,8 @@ export const PORTFOLIO_DATA: PortfolioData[] = [
     url: null,
     gitHubLink: null,
     title: 'Man Space Mobile App',
-    content: '',
+    content:
+      'A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.',
     tags: ['Typescript', 'React-Native', 'Nodejs'],
   },
 ];
