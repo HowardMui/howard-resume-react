89a29c9095c51644b8908a8666ce7cf8ee48eea6
HowardMui <howardmui.2020@gmail.com>
2023-05-17 03:11:21 +0800

    feat: add gif in apps, home container

--- src/app/pages/HomeContainer/HomeContainer.tsx
+++ src/app/pages/HomeContainer/HomeContainer.tsx
@@ -1,9 +1,25 @@
 import 'assets/scss/Containers/HomeContainer/HomeContainer.scss';
-import { useMotionValueEvent, useScroll, motion } from 'framer-motion';
+import {
+  useMotionValueEvent,
+  useScroll,
+  motion,
+  useAnimate,
+  useInView,
+} from 'framer-motion';
 import { ReactComponent as CpuIcon } from 'assets/icons/home-cpu-no-clock.svg';
-import { Col, Row } from 'react-bootstrap';
+// import CodingGif from 'assets/gif/coding_cpu.gif';
+import DesktopGif from 'assets/gif/desktop-nobg.gif';
+import OldDesktopGif from 'assets/gif/old_desktop_9.gif';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { useOnScreen } from 'hooks/useOnScreen';
+import { NavBarType } from 'models';
 
-const HomeContainer = props => {
+interface Props {
+  setActive: (value: NavBarType | null) => void;
+}
+
+const HomeContainer = ({ setActive }: Props) => {
   // const options = useMemo(() => {
   //   return {
   //     particles: {
@@ -20,26 +36,84 @@ const HomeContainer = props => {
   //   };
   // }, []);
 
+  // const ref = useRef<HTMLDivElement>(null);
+  // const isVisible = useOnScreen(ref);
+
+  // useEffect(() => {
+  //   // console.log('home', isVisible);
+  //   // if (isVisible) {
+  //   //   setActive(null);
+  //   // }
+  // }, [isVisible, setActive]);
+
+  // const [scope, animate] = useAnimate();
+  // const isInView = useInView(scope);
+
+  // useEffect(() => {
+  //   // console.log(idName, isInView);
+  //   // console.log(isInView);
+  //   if (isInView) {
+  //     setActive(null);
+  //   }
+  // }, [animate, isInView, scope, setActive]);
+
+  const myDivRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        rootMargin: '0px',
+        threshold: 0.5,
+      },
+    );
+
+    if (myDivRef.current) {
+      observer.observe(myDivRef.current);
+    }
+
+    return () => {
+      if (myDivRef.current) {
+        observer.unobserve(myDivRef.current);
+      }
+    };
+  }, [myDivRef]);
+
+  useMemo(() => {
+    if (isVisible) {
+      setActive(null);
+    } else {
+      setActive(null);
+    }
+  }, [isVisible, setActive]);
+
   return (
-    <div className="home-container">
-      <Row>
-        <Col lg={6} className="left-div d-flex align-items-center gy-3">
-          <div className="content-wrapper">
-            <div className="job-title ">FRONTEND DEVELOPER</div>
-            <h1 className="my-name">Howrad Mui</h1>
-            <div className="fs-5 text-muted">
-              A Person who is about Programming and aspired to ba a full stack
-              developer
+    <div className="home-container" ref={myDivRef}>
+      <Container>
+        <Row>
+          <Col lg={6} className="left-div d-flex align-items-center">
+            <div className="content-wrapper">
+              <div className="job-title ">FRONTEND DEVELOPER</div>
+              <h1 className="my-name">Howrad Mui</h1>
+              <div className="fs-5 text-muted">
+                A programming enthusiast with the aspiration of being a
+                full-stack developer
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col
-          lg={6}
-          className="right-div d-flex justify-content-end align-items-end "
-        >
-          <CpuIcon width={450} height={450} />
-        </Col>
-      </Row>
+          </Col>
+          <Col
+            lg={6}
+            className="right-div d-flex justify-content-end align-items-end "
+          >
+            {/* <CodingGif width={450} height={450} /> */}
+            {/* <img src={DesktopGif} alt="desktop" className="gif-div" /> */}
+            <img src={OldDesktopGif} alt="desktop" className="gif-div" />
+          </Col>
+        </Row>
+      </Container>
 
       <div className="link-to-about xs:bottom-10 bottom-32">
         <a href="#about">
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,6 +2,7 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg';
+declare module '*.gif';
 
 declare namespace JSX {
   interface IntrinsicElements {
