bdc0a56ef4da780ebba40faafb33a51d21d65918
HowardMui <howardmui.2020@gmail.com>
2023-05-18 02:53:28 +0800

    feat: add toastContainer in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import 'react-vertical-timeline-component/style.min.css';
 import ReactDOM from 'react-dom/client';
 import Modal from 'react-modal';
 import 'theme/index.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -15,5 +17,6 @@ Modal.setAppElement('#root');
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>,
 );
