865220d5e10c8a227daceada64be909258a523b4
HowardMui <howardmui.2020@gmail.com>
2023-05-18 02:52:07 +0800

    feat: add emailjs in ContactformCard

--- src/app/components/Contact/ContactFormCard.tsx
+++ src/app/components/Contact/ContactFormCard.tsx
@@ -1,42 +1,76 @@
 import { Form, Formik } from 'formik';
-import { useState } from 'react';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { useCallback, useEffect, useState } from 'react';
+import { Button, Col, Row, Spinner } from 'react-bootstrap';
 import * as Yup from 'yup';
 import { InputField } from '../Form';
 import 'assets/scss/Components/Contact/ContactFormCard.scss';
-import SendingGif from 'assets/gif/sending_trans_30.gif';
+// import SendingGif from 'assets/gif/sending_trans_30.gif';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-toastify';
 
 // form type
 export type FormItem = {
   name: string;
   email: string;
-  title: string;
-  number: number;
-  contact: string;
+  message: string;
 };
 
 // form initialValues
 const FORM_ITEM = {
   name: '',
   email: '',
-  title: '',
-  number: 0,
-  contact: '',
+  message: '',
 };
 
 const SCHEMA = Yup.object({
-  // oldPassword: Yup.string().required('必填項目'),
-  // newPassword: Yup.string().required('必填項目'),
-  // confirmNewPassword: Yup.string()
-  //   .required('必填項目')
-  //   .oneOf([Yup.ref('newPassword')], '密碼不相同'),
+  name: Yup.string().max(30, 'Maximum 30 characters').required('Required'),
+  email: Yup.string().email('Please input valid email.').required('Required'),
+  message: Yup.string()
+    .min(10, 'At least 10 characters')
+    .max(200, 'Maximum 200 characters')
+    .required('Required'),
 });
 
 const ContactFormCard = () => {
   const [form, setForm] = useState<FormItem>(FORM_ITEM);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const onSubmit = () => {};
+  // const resetForm = useCallback(() => {
+  //   setForm({
+  //     name: '',
+  //     email: '',
+  //     message: '',
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   resetForm();
+  // }, [resetForm]);
+
+  const onSubmit = (values: FormItem) => {
+    setLoading(true);
+
+    const { name, email, message } = values;
+    const params = {
+      from_name: name,
+      email_id: email,
+      message,
+    };
+
+    emailjs
+      .send('service_oipuaat', 'template_ovu66h6', params, 'qqkoKcjfSaGUgBB6e')
+      .then(
+        result => {
+          toast.success('Submitted Successfully!');
+          setLoading(false);
+        },
+        error => {
+          console.log(error);
+          toast.error('Failed to submit.  Please try again.');
+          setLoading(false);
+        },
+      );
+  };
 
   return (
     <div className="cus-card">
@@ -55,23 +89,31 @@ const ContactFormCard = () => {
               <Col md={12}>
                 <InputField name="email" placeholder="Email" />
               </Col>
-              {/* <Col md={12}>
-                <InputField name="title" placeholder="Subject" />
-              </Col> */}
               <Col md={12}>
                 <InputField
-                  name="content"
+                  name="message"
                   type="textarea"
                   placeholder="Message"
                 />
               </Col>
             </Row>
             <div className="mt-3 text-center">
-              <Button type="submit" className="ms-1" disabled={loading}>
+              <Button
+                type="submit"
+                className="ms-1 "
+                disabled={loading}
+                style={{ margin: 'auto', width: '80px' }}
+              >
                 {loading ? (
-                  <div>Loading</div>
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
                 ) : (
-                  <img src={SendingGif} alt="sending" width={60} height={40} />
+                  'Submit'
                 )}
               </Button>
             </div>
